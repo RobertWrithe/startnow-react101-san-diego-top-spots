a008785dfc12f975cd745c1b8515c6ad53b93907
Robert Barnett <therobertbarnett@gmail.com>
2018-09-05 15:21:04 -0700

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+PROJECT:
+
+React San Diego Top Spots
+
+DESCRIPTION:
+
+This project is a Reactjs experiment in pulling JSON data into object array to populate a table for public reading. The last column holds a link that opens to the top spot's location on Google Maps using given coordinates from the JSON data.
+
+INSTRUCTIONS:
+
+1. Start program from terminal and view at localhost.
+2. Learn about some of San Diego's trendy, over-hyped top spots (though some of this information is out of date).
+3. Click the Google link to find the spot's location!
+4. Have fun, you tourist!
---
+++ src/app.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+import TopSpot from './topspot';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      topspots: []
+    };
+  }
+
+  componentDidMount() {
+    // Here we're simply invoking the axios object, specifically.. 
+    Axios
+
+      // .. the .get function. Here we pass the URL of our external service.
+      .get('https://origin-top-spots-api.herokuapp.com/api/topspots')
+
+      // Here the first fulfill callback is setup.
+      // This callback simply takes an HTTP response and returns the data property..
+      .then(response => response.data)
+
+      // .. to the second fulfill callback, which uses React's this.setState
+      // function to merge the provided object (which uses ES6 shorthand)
+      // with the current object assigned to this.state (which you assigned 
+      // in your constructor!)
+      .then(topspots => this.setState({ topspots }));
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <div className='container center-block'>
+          <div className='jumbotron'>
+            <strong><h1>San Diego Top Spots</h1></strong>
+            <p>A list of the top 30 places to see in San Diego, California</p>
+          </div>
+          {this.state.topspots.map(topspot => (
+            <TopSpot
+              key={topspot.id}
+              name={topspot.name}
+              description={topspot.description}
+              location={topspot.location} />
+          ))}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
